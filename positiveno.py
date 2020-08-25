f6c70106a316d37c1d324337afedcd2ad8e4fccd
Amita  chorghe <67407962+amitachorghe@users.noreply.github.com>
2020-08-25 23:12:02 +0530

    Create positiveno.py
    output of two list in one program

---
+++ positiveno.py
@@ -0,0 +1,13 @@
+#Positive no in list
+list1 = [12,-7,5,64,-14]
+list2 = [12,14,-95,3]
+
+print("The positive no from list1 :")
+for num in list1:
+    if num>=0:
+        print(num)
+
+print("The positive no from list2 :")
+for num in list2:
+    if num>=0:
+        print(num)
